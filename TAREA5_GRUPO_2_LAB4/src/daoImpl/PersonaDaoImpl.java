083481d5410261c29ffd4177a6b6fa49621caedd
orianagarcia <orugarcia@gmail.com>
2020-05-16 18:05:25 -0300

    v2

--- TAREA5_GRUPO_2_LAB4/src/daoImpl/PersonaDaoImpl.java
+++ TAREA5_GRUPO_2_LAB4/src/daoImpl/PersonaDaoImpl.java
@@ -1,10 +1,15 @@
 package daoImpl;
 
 import java.sql.Connection;
+import java.util.List;
+
+import dao.PersonaDao;
+import entidad.Persona;
 
 public class PersonaDaoImpl implements PersonaDao{
 	
-	private static final String insert = "INSERT INTO personas(dni, nombre, apellido) VALUES(?, ?, ?)";
-	private static final String delete = "DELETE FROM personas WHERE dni = ?";
-	private static final String readall = "SELECT * FROM personas";
+	//private static final String insert = "INSERT INTO personas(dni, nombre, apellido) VALUES(?, ?, ?)";
+	//private static final String delete = "DELETE FROM personas WHERE dni = ?";
+	//private static final String readall = "SELECT * FROM personas";
+
 }
